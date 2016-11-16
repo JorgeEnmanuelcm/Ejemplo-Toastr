125f7b7b0bef10d1d21fbc4582a2fef2f7f3f351
Jorge Enmanuel Castro Mendez <georgeecm@outlook.com>
2016-11-16 04:43:23 +0100

    ....

---
+++ ratodelab/WebForm2.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace ratodelab
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void NuevoButton_Click(object sender, EventArgs e)
+        {
+
+           utilitarios.ShowToastr(this, "bien", "Se limpio con exito!", "success");
+        }
+
+        protected void EliminarButton_Click(object sender, EventArgs e)
+        {
+            utilitarios.ShowToastr(this, "bien", "Se elimino con exito!", "success");
+        }
+
+        protected void GuardarButton_Click(object sender, EventArgs e)
+        {
+            utilitarios.ShowToastr(this, "bien", "Se guardo con exito!", "success");
+        }
+
+        protected void BuscarButton_Click(object sender, EventArgs e)
+        {
+            utilitarios.ShowToastr(this, "error", "Mensaje", "error");
+        }
+    }
+}
